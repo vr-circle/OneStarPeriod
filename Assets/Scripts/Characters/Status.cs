04d3f633b3fb0464c4df65207e815c6e3048cdd0
yuusuine <yuuki.990510@gmail.com>
2020-03-02 13:45:07 +0900

    commit

---
+++ Assets/Scripts/Characters/Status.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Character
+{
+	public class Status
+	{
+		private float hp;
+		//private float mp;
+
+
+		public void GetDamage(float damage)
+		{
+			hp -= damage;
+		}
+
+
+
+	}
+}
