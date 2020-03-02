0c8e76d6b7e6e9e14eb3e2dcae695ad4e9bc0030
ATtami YYNa <tami19996@gmail.com>
2020-03-02 23:20:19 +0900

    Bot,Enemy,support

---
+++ Assets/Scripts/Bot/BotLooking.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotLooking : MonoBehaviour
+{
+    [SerializeField] GameObject Bot;
+    [SerializeField] GameObject Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //BotRigid.transform.LookAt(Target.transform);
+
+        float speed = 0.1f;
+
+        Vector3 relativePos = Target.transform.position - this.transform.position;
+
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+        transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation, speed);
+    }
+}
---
+++ Assets/Scripts/Bot/BotShooting.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotShooting : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject Botbullet;
+
+	private float bulletSpeed = 20.0f;
+
+	private int bulletNum = 100000;
+
+	private float positionMargin = 1.0f;
+
+
+
+	private bool isShooted = false;
+	private float timeInterval = 1.0f;
+	private float elapsedTime = 0.0f;
+
+	//public Rect BulletNumDispPos = new Rect(0, 0, 100, 50);  // 場所は後で決める
+
+
+	private void Update()
+	{
+		if (isShooted)
+		{
+			elapsedTime += Time.deltaTime;
+
+
+			if (elapsedTime > timeInterval)
+			{
+				isShooted = false;
+
+				elapsedTime = 0.0f;
+
+			}
+		}
+		Shoot();
+	}
+
+
+	private void Shoot()
+	{
+		if (!isShooted)
+		{
+
+			if (bulletNum > 0)
+			{
+
+				isShooted = true;
+
+
+				Vector3 startPosition = this.transform.position;
+
+				startPosition += this.transform.forward * positionMargin;
+
+				GameObject bulletTmp = Instantiate(Botbullet, startPosition, this.transform.rotation);
+
+				bulletTmp.GetComponent<Rigidbody>().velocity = this.transform.forward * bulletSpeed;
+
+				bulletNum--;
+			}
+			else
+			{
+				//弾切れ
+			}
+		}
+	}
+}
