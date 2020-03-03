011a0699f5eea7ffd45eb55a8de4fad0fd31837c
yuusuine <yuuki.990510@gmail.com>
2020-03-04 00:28:54 +0900

    create title and connect scenes

---
+++ Assets/Scripts/Title/InputDirector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace OneStarPeriod
+{
+	namespace TitleScene
+	{
+		public class InputDirector : MonoBehaviour
+		{
+			private void Update()
+			{
+				if ((Input.GetKeyDown(KeyCode.KeypadEnter)) || (Input.GetMouseButtonDown(0)))
+				{
+					SceneManager.LoadScene("StageSelect");
+				}
+			}
+		}
+	}
+}
